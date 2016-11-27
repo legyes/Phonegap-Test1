43793d5cb6c3a03e2ddb641c00d9de68fe04ba6b
legyes <legyes@legyes.hu>
2016-11-27 17:50:52 +0100

    cond commit

---
+++ www/js/my-app.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+    $('#link_refresh').on('click',function(){
+        refreshRSS();
+    });
+
+});
+
+function refreshRSS() {
+    $('#news_fresh').html('');
+    $.post("http://www.p1race.hu/rss/rss.php",{}, function(rss) {
+
+        $(rss).find('item').each(function(i, item){
+            $('#news_fresh').append(
+                '<div>' +
+                    '<div><b>'+ $(item).find('title').text() +'</b></div>' +
+                    '<div>'+ $(item).find('description').text() +'</div>' +
+                '</div>');
+            });
+
+    },'xml');
+
+}
