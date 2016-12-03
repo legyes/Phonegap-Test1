e9bcc81060fb58fd7d1b28789d464d7070779826
LeGyEs <legyes@legyes.hu>
2016-12-03 22:20:06 +0100

    16.

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,5 +1,31 @@
+try {
+    var permissions = cordova.plugins.permissions;
+    var networkState = navigator.connection.type;
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+
+}
+catch (err) {
+    //alert(err);
+    // pre Android 6.0 hack
+}
+
 $(document).ready(function(){
+
+    if (typeof(Storage) !== "undefined") {
+    } else {
+    }
+
+    //checkConnection();
     refreshRSS();
+    refreshCategories();
 
     $('#link_refresh').on('click',function(){
         refreshRSS();
@@ -9,18 +35,41 @@ $(document).ready(function(){
         $('#news_fresh').html('Gomb lenyomva');
     });
 
+    // side menu
+    $('#a_menu').on('click',function(){
+
+        if ( $('aside').hasClass('closed') ) {
+            $('aside').animate({
+                width: '300px'
+            }).addClass('open').removeClass('closed');
+        }
+        else {
+            $('aside').animate({
+                width: '0px'
+            }).addClass('closed').removeClass('open');
+        }
+
+    });
+
 });
 
-
+function checkConnection() {
+    try {
+        $('#indicator_net').html('<i class="fa fa-wifi"></i> ' + networkState);
+    }    
+    catch(err) {
+        alert(err);
+    }
+}
 
 function refreshRSS() {
     $('#news_fresh').html('');
-    $.post("http://www.p1race.hu/api/articles/",{}, function(articles) {
-        $(articles).each(function(i, article){
+    $.post("http://www.p1race.hu/api/articles/",{}, function(data) {
+        $(data.articles).each(function(i, article){
             $('#news_fresh').append(
                 '<article style="background-color: #ffffff; border-radius: 4px; padding: 1px 10px 5px; margin: 25px 0px; box-shadow: 0px 0px 20px grey;" data-target="http://www.p1race.hu/hir/'+ article.Slug +'">' +
                     '<div><h3>'+ article.Title +'</h3></div>' +
-                    '<div style="position: relative; margin: 10px -10px;">' +
+                    '<div style="position: relative; margin: 10px -10px; min-height: 100px;">' +
                         '<img src="http://www.p1race.hu/'+ article.Image +'" style="width: 100%;" />' +
                         '<div style="position: absolute; top: 20px; left: 0px; background-color: #94D34A; color: #ffffff; padding: 5px; font-weight: bold; text-shadow: 1px 1px 2px black;">'+ article.CategoryName +'</div>' +
                     '</div>' +
@@ -40,9 +89,48 @@ function refreshRSS() {
 
 }
 
-var permissions = cordova.plugins.permissions;
+function refreshCategories() {
+    c = $('#aside_menu');
+    c.html('<div class="col-md-12 text-center"><select id="select_category" class="form-control"></select></div>');
+
+    s = c.find('select');
+
+    $.post('http://www.p1race.hu/api/categories',{},function(data){
+        $(data.categories).each(function(i, category){
+
+             s.append('<option value="'+ category.ID +'">'+ category.Name +'</option>');
+
+        });
+
+        c.off('change').on('change', function(){
+            $('#modal_menu').modal('hide');
+        });
+
+    },'json');
+
+}
+
 /*
 permissions.hasPermission(permission, successCallback, errorCallback);
 permissions.requestPermission(permission, successCallback, errorCallback);
 permissions.requestPermissions(permissions, successCallback, errorCallback);
+*/
+
+
+// confirm teszt
+/*
+navigator.notification.confirm(
+    networkState + ' kapcsolat. Frissíted a híreket?', // message
+     onConfirm,            // callback to invoke with index of button pressed
+    'Figylem!',           // title
+    ['Igen','Nem']     // buttonLabels
+);
+
+function onConfirm(buttonIndex) {
+    if ( buttonIndex === 1 ) {
+         refreshRSS();
+    }
+}
+
+
 */
