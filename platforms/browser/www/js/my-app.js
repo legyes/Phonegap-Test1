d9b999820bd6b4d15dc8e4d8f3565b358712d106
LeGyEs <legyes@legyes.hu>
2016-12-03 23:31:12 +0100

    22.

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -65,33 +65,32 @@ function checkConnection() {
 function refreshRSS() {
 
     if ( typeof window.localStorage.getItem('articles' !== null)) {
-        alert('articles OK');
         articles = JSON.parse( window.localStorage.getItem('articles') );
         $('#news_fresh').html('');
 
         $.when(
             $.each(articles, function(i, article){
+                alert(article.Title);
             $('#news_fresh').append(
-                    '<article style="background-color: #ffffff; border-radius: 4px; padding: 1px 10px 5px; margin: 25px 0px; box-shadow: 0px 0px 20px grey;" data-target="http://www.p1race.hu/hir/'+ article.Slug +'">' +
-                        '<div><h3>'+ article.Title +'</h3></div>' +
-                        '<div style="position: relative; margin: 10px -10px; min-height: 100px;">' +
-                            '<img id="image_'+ article.ID +'" src="http://www.p1race.hu/'+ article.Image +'" style="width: 100%;" />' +
-                            '<div style="position: absolute; top: 20px; left: 0px; background-color: #94D34A; color: #ffffff; padding: 5px; font-weight: bold; text-shadow: 1px 1px 2px black;">'+ article.CategoryName +'</div>' +
-                        '</div>' +
-                        '<div class="text-left" style="font-size: 1.2em;">'+ article.Article +'</div>' +
-                        '<div class="row" style="color: #aaaaaa; padding: 5px 0px;">'+ 
-                            '<div class="col-xs-5"><i class="fa fa-pencil"></i> ' + article.Creator + '</div>' +
-                            '<div class="col-xs-4 text-center"><i class="fa fa-calendar"></i> ' + article.TimeAgo + '</div>' +
-                            '<div class="col-xs-3 text-right"><i class="fa fa-comments"></i> ' + article.CommentCount + '</div>' +
-                        '</div>' +
-                    '</article>');
+                '<article style="background-color: #ffffff; border-radius: 4px; padding: 1px 10px 5px; margin: 25px 0px; box-shadow: 0px 0px 20px grey;" data-target="http://www.p1race.hu/hir/'+ article.Slug +'">' +
+                    '<div><h3>'+ article.Title +'</h3></div>' +
+                    '<div style="position: relative; margin: 10px -10px; min-height: 100px;">' +
+                        '<img id="image_'+ article.ID +'" src="http://www.p1race.hu/'+ article.Image +'" style="width: 100%;" />' +
+                        '<div style="position: absolute; top: 20px; left: 0px; background-color: #94D34A; color: #ffffff; padding: 5px; font-weight: bold; text-shadow: 1px 1px 2px black;">'+ article.CategoryName +'</div>' +
+                    '</div>' +
+                    '<div class="text-left" style="font-size: 1.2em;">'+ article.Article +'</div>' +
+                    '<div class="row" style="color: #aaaaaa; padding: 5px 0px;">'+ 
+                        '<div class="col-xs-5"><i class="fa fa-pencil"></i> ' + article.Creator + '</div>' +
+                        '<div class="col-xs-4 text-center"><i class="fa fa-calendar"></i> ' + article.TimeAgo + '</div>' +
+                        '<div class="col-xs-3 text-right"><i class="fa fa-comments"></i> ' + article.CommentCount + '</div>' +
+                    '</div>' +
+                '</article>');
             })
         ).done(function(){
             refreshRSSOnline();
         });
     }
     else {
-        alert('articles :(');
         refreshRSSOnline();
     }
 
