a7c5619b2f8b69c8c18fe3733090a0bda296a4d9
LeGyEs <legyes@legyes.hu>
2016-12-03 23:10:19 +0100

    18. sqlite storage plugin fix

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -73,7 +73,7 @@ function checkConnection() {
 
 function refreshRSS() {
 
-    if ( typeof window.localStorage.articles !== 'undefined' ) {
+    if ( typeof window.localStorage.getItem('articles' !== null)) {
         articles = JSON.parse( window.localStorage.articles );
         $('#news_fresh').html('');
         $.each(articles, function(i, article){
@@ -92,45 +92,49 @@ function refreshRSS() {
                     '</div>' +
                 '</article>');
         }).promise().done(function(){
-        // --------------------------
-            $('#news_fresh').html('');
-                $.post("http://www.p1race.hu/api/articles/",{}, function(data) {
-                    cache_articles = data.articles;
-                    $(data.articles).each(function(i, article){
-
-                        window.localStorage.setItem('article_'+article.ID, JSON.stringify(article));
-
-                        $('#news_fresh').append(
-                            '<article style="background-color: #ffffff; border-radius: 4px; padding: 1px 10px 5px; margin: 25px 0px; box-shadow: 0px 0px 20px grey;" data-target="http://www.p1race.hu/hir/'+ article.Slug +'">' +
-                                '<div><h3>'+ article.Title +'</h3></div>' +
-                                '<div style="position: relative; margin: 10px -10px; min-height: 100px;">' +
-                                    '<img id="image_'+ article.ID +'" src="http://www.p1race.hu/'+ article.Image +'" style="width: 100%;" />' +
-                                    '<div style="position: absolute; top: 20px; left: 0px; background-color: #94D34A; color: #ffffff; padding: 5px; font-weight: bold; text-shadow: 1px 1px 2px black;">'+ article.CategoryName +'</div>' +
-                                '</div>' +
-                                '<div class="text-left" style="font-size: 1.2em;">'+ article.Article +'</div>' +
-                                '<div class="row" style="color: #aaaaaa; padding: 5px 0px;">'+ 
-                                    '<div class="col-xs-5"><i class="fa fa-pencil"></i> ' + article.Creator + '</div>' +
-                                    '<div class="col-xs-4 text-center"><i class="fa fa-calendar"></i> ' + article.TimeAgo + '</div>' +
-                                    '<div class="col-xs-3 text-right"><i class="fa fa-comments"></i> ' + article.CommentCount + '</div>' +
-                                '</div>' +
-                            '</article>');
-                        });
-
-                },'json').done(function(){
-
-                    window.localStorage.setItem('articles', JSON.stringify(cache_articles));
-
-
-                    $('article').off('click').on('click',function() {
-                        window.open( $(this).data('target'), '_system');
-                    });
-                });
-
-        // --------------------------
+            refreshRSSOnline();
         });
+    }
+    else {
+        refreshRSSOnline();
     }
 
  }
+
+function refreshRSSOnline() {
+    $('#news_fresh').html('');
+        $.post("http://www.p1race.hu/api/articles/",{}, function(data) {
+            cache_articles = data.articles;
+            $(data.articles).each(function(i, article){
+
+                window.localStorage.setItem('article_'+article.ID, JSON.stringify(article));
+
+                $('#news_fresh').append(
+                    '<article style="background-color: #ffffff; border-radius: 4px; padding: 1px 10px 5px; margin: 25px 0px; box-shadow: 0px 0px 20px grey;" data-target="http://www.p1race.hu/hir/'+ article.Slug +'">' +
+                        '<div><h3>'+ article.Title +'</h3></div>' +
+                        '<div style="position: relative; margin: 10px -10px; min-height: 100px;">' +
+                            '<img id="image_'+ article.ID +'" src="http://www.p1race.hu/'+ article.Image +'" style="width: 100%;" />' +
+                            '<div style="position: absolute; top: 20px; left: 0px; background-color: #94D34A; color: #ffffff; padding: 5px; font-weight: bold; text-shadow: 1px 1px 2px black;">'+ article.CategoryName +'</div>' +
+                        '</div>' +
+                        '<div class="text-left" style="font-size: 1.2em;">'+ article.Article +'</div>' +
+                        '<div class="row" style="color: #aaaaaa; padding: 5px 0px;">'+ 
+                            '<div class="col-xs-5"><i class="fa fa-pencil"></i> ' + article.Creator + '</div>' +
+                            '<div class="col-xs-4 text-center"><i class="fa fa-calendar"></i> ' + article.TimeAgo + '</div>' +
+                            '<div class="col-xs-3 text-right"><i class="fa fa-comments"></i> ' + article.CommentCount + '</div>' +
+                        '</div>' +
+                    '</article>');
+                });
+
+        },'json').done(function(){
+
+            window.localStorage.setItem('articles', JSON.stringify(cache_articles));
+
+
+            $('article').off('click').on('click',function() {
+                window.open( $(this).data('target'), '_system');
+            });
+        });    
+}
 
 function refreshCategories() {
     c = $('#aside_menu');
